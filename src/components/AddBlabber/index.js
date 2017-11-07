e5f731dfafd0d758c8dde9fdd57e53e923360388
Emil Björling <emilbjorling@Emils-MacBook-Pro.local>
2017-11-07 20:13:46 +0100

    Added new component to add a blabber

---
+++ src/components/AddBlabber/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class AddBlabber extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <button>+</button>
+      </div>
+    )
+  }
+}
+
+export default AddBlabber
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Header from '../Header'
+import AddBlabber from '../AddBlabber'
 
 import './style.css'
 
@@ -9,7 +10,7 @@ class App extends React.Component {
     return (
       <div className="container">
         <Header />
-        APP GOES HERE!
+        <AddBlabber />
       </div>
     )
   }
